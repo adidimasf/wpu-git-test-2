00df1a851d2f6b2647ffec16cb6efdd7bb593acf
adidimasf <adidimasf@gmail.com>
2025-11-17 21:13:49 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Heloo world</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab minima delectus nisi molestiae dolor modi animi. Repudiandae quam dignissimos, non vero velit iure tempore eveniet porro qui hic itaque consectetur!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
